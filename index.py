af339ae68ea42618115a679fb4e01c2f168bffe9
Borychev Andrey <borychev.au@gmail.com>
2025-04-26 13:42:43 +0300

    удалил лишний файл

--- index.py
+++
@@ -1,4 +0,0 @@
-kindPet = input('Введите вид питомца: ')
-agePet = input('Введите возраст питомца: ')
-namePet = input('Введите кличку питомца: ')
-print('Это', kindPet, 'по кличке', namePet, 'Возраст:', agePet, '.')
