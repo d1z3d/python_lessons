b6f9c3c256673c041ad1e4c5faa4ee646a7b6ad7
Borychev Andrey <borychev.au@gmail.com>
2025-05-07 23:05:55 +0300

    внесение правок

--- task2.py
+++ task2.py
@@ -1,6 +1,17 @@
 # Задание №2
 
-numbers1 = set(map(int, input("Введите числа первого списка через пробел: ").split()))
-numbers2 = set(map(int, input("Введите числа второго списка через пробел: ").split()))
+# Вводятся два списка чисел, которые могут содержать до 100000 чисел каждый. Все числа каждого списка находятся на отдельной строке. Выведите, сколько чисел содержится одновременно как в первом списке, так и во втором.
+
+
+def read_valid_numbers(prompt):
+    while True:
+        numbers = list(map(int, input(prompt).split()))
+        if len(numbers) > 100000:
+            print("Слишком много чисел. Введите не более 100000.")
+        else:
+            return set(numbers)
+
+numbers1 = read_valid_numbers("Введите числа первого списка через пробел: ")
+numbers2 = read_valid_numbers("Введите числа второго списка через пробел: ")
 common_numbers = numbers1.intersection(numbers2)
 print("Количество чисел, содержащихся в обоих списках:", len(common_numbers))
