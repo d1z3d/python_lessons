39462458c7ea0f21db1dfb4f09abe9cb97f22d4d
Borychev Andrey <borychev.au@gmail.com>
2025-05-07 23:28:53 +0300

    выполнил урок 10

--- task1.py
+++ task1.py
@@ -1,11 +1,28 @@
 # Задание №1
 
-N = int(input("Введите количество чисел от 1 до 100000: "))
+def get_age_suffix(age):
+    if age % 10 == 1 and age % 100 != 11:
+        return "год"
+    elif age % 10 in [2, 3, 4] and age % 100 not in [12, 13, 14]:
+        return "года"
+    else:
+        return "лет"
+    
 
-numbers = map(int, input("Введите числа через пробел: ").split())
-if len(numbers) != N:
-    print("Количество чисел не совпадает с введенным N.")
-    exit()
+pets = {}
 
-unique_numbers = set(numbers)
-print("Количество различных чисел:", len(unique_numbers))
+name = input("Введите имя питомца: ")
+animal_type = input("Введите вид питомца: ")
+age = int(input("Введите возраст питомца: "))
+owner_name = input("Введите имя владельца: ")
+
+pets[name] = {
+    "Вид питомца": animal_type,
+    "Возраст питомца": age,
+    "Имя владельца": owner_name
+}
+
+for key, value in pets.items():
+    age_suffix = get_age_suffix(value["Возраст питомца"])
+    print(f"Это {value['Вид питомца']} по кличке \"{key}\". Возраст питомца: {value['Возраст питомца']} {age_suffix}. Имя владельца: {value['Имя владельца']}")
+
--- task2.py
+++ task2.py
@@ -1,17 +1,6 @@
 # Задание №2
 
-# Вводятся два списка чисел, которые могут содержать до 100000 чисел каждый. Все числа каждого списка находятся на отдельной строке. Выведите, сколько чисел содержится одновременно как в первом списке, так и во втором.
+numbers= {}
 
-
-def read_valid_numbers(prompt):
-    while True:
-        numbers = list(map(int, input(prompt).split()))
-        if len(numbers) > 100000:
-            print("Слишком много чисел. Введите не более 100000.")
-        else:
-            return set(numbers)
-
-numbers1 = read_valid_numbers("Введите числа первого списка через пробел: ")
-numbers2 = read_valid_numbers("Введите числа второго списка через пробел: ")
-common_numbers = numbers1.intersection(numbers2)
-print("Количество чисел, содержащихся в обоих списках:", len(common_numbers))
+for number in range(10, -6, -1):
+    numbers[number] = number ** number
